ee17b4eeb82d6a50b211aa89465e997fa53d435d
Moudy <mood93@Mahmouds-MacBook-Air.local>
2018-01-07 12:49:45 -0600

    Initial Commit - Pre-Styling

---
+++ js/Get-It-Done-jQuery.js
@@ -0,0 +1,28 @@
+//Check off todos by clicking
+$("ul").on("click", "li", function() {
+    
+    $(this).toggleClass("completed");
+    
+})
+
+//Click X to delete
+$("ul").on("click", "span", function(event) {
+    
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    })
+    event.stopPropagation();
+    
+})
+
+$("input[type= 'text']").keypress(function(event) {
+    
+    if(event.which === 13){
+        //grab new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new lie and add to ul
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+    }
+    
+});
