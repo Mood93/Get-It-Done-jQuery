9df660b3f68a3f8c317e1d0f1d94199a48f0dd74
Moudy <mood93@Mahmouds-MacBook-Air.local>
2018-01-07 18:49:11 -0600

    Added Styling v2
    CSS Gradient
    Plus icon toggle
    Delete icon toggle
    FontAwesome 5

--- js/Get-It-Done-jQuery.js
+++ js/Get-It-Done-jQuery.js
@@ -1,28 +1,32 @@
 //Check off todos by clicking
-$("ul").on("click", "li", function() {
+$("ul").on("click", "li", function () {
     
     $(this).toggleClass("completed");
-    
-})
+});
 
 //Click X to delete
-$("ul").on("click", "span", function(event) {
+$("ul").on("click", "span", function (event) {
     
-    $(this).parent().fadeOut(500, function() {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
-    })
+    });
     event.stopPropagation();
-    
-})
+});
 
-$("input[type= 'text']").keypress(function(event) {
+
+
+$("input[type= 'text']").keypress(function (event) {
     
-    if(event.which === 13){
+    if (event.which === 13) {
         //grab new todo text from input
         var todoText = $(this).val();
         $(this).val("");
         // create a new lie and add to ul
-        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+        $("ul").append("<li><span><i class='far fa-trash-alt'></span>" + todoText + "</li>");
     }
-    
+});
+
+
+$(".add").click(function () {
+    $("input[type= 'text']").fadeToggle();
 });
